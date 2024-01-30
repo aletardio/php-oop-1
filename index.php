a3a127b5338c2cef2a1d302a515b5adc3c87658c
aletardio <ale.tardio99@gmail.com>
2024-01-30 15:23:51 +0100

    created methods

--- index.php
+++ index.php
@@ -4,11 +4,24 @@
         public $name;
         public $genre;
         public $year;
+        public $discount = 0;
 
         function __construct($_name, $_genre, $_year) {
             $this->name = $_name;
             $this->genre = $_genre;
             $this->year = $_year;
+        }
+
+        public function setDiscount($age){
+            if($age  >= 25 && $age <= 65){
+                $this->discount = 10;
+            } elseif ($age > 65) {
+                $this->discount = 25;
+            }
+        }
+
+        public function getDiscount() {
+            return $this->discount;
         }
     }
 
