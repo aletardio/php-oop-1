027eb5ba28afcfdea9a51426601499dd25d81681
aletardio <ale.tardio99@gmail.com>
2024-01-30 15:18:17 +0100

    Added function construct

--- index.php
+++ index.php
@@ -4,6 +4,12 @@
         public $name;
         public $genre;
         public $year;
+
+        function __construct($_name, $_genre, $_year) {
+            $this->name = $_name;
+            $this->genre = $_genre;
+            $this->year = $_year;
+        }
     }
 
 
